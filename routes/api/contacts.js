01ae44690201aad4d1551bcc832b679c60e6d972
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-09-05 18:40:30 +0800

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+// http://localhost:3000/api/contacts/
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error);
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
