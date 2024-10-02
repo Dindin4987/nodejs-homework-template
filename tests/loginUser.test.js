6f4aeb229d84b9d0e6d58b24264d0631ee729d33
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-10-02 20:42:59 +0800

    update

--- tests/loginUser.test.js
+++ tests/loginUser.test.js
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../app.js";
 import { User } from "../models/usersModel.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypntjs";
 import jwt from "jsonwebtoken";
 import { jest } from "@jest/globals";
 
